51a9a594da6433f89c3ffdb85028d5679ebeb15d
Roy Wu <roy32011@gmail.com>
2023-12-21 09:53:42 +0800

    Create base template

--- job_application/views.py
+++ job_application/views.py
@@ -26,3 +26,6 @@ def index(request):
             messages.success(request, "Form submitted  successfully!")
 
     return render(request, template_name="index.html")
+
+def about(request):
+    return render(request, template_name="about.html")
